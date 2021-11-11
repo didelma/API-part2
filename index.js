96e8e4a604ae0d519e04eb359ddf41a3b2184bad
Jenny <jennyd.moncion@gmail.com>
2021-11-10 19:57:11 -0500

    shit better work

---
+++ index.js
@@ -0,0 +1,11 @@
+import axios from 'axios';
+import dotenv from 'dotenv';
+dotenv.config();
+
+
+const button = document.querySelector("button");
+const giphyInput = document.querySelector("input");
+
+button.addEventListener('click', async () => {
+
+})
